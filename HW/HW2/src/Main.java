b8844df456abaddc7d0f80985e5976ed7a5ed59c
vika <vmliechko@mail.ru>
2024-07-11 16:00:06 +0300

    change hw2

--- HW/HW2/src/Main.java
+++ HW/HW2/src/Main.java
@@ -1,11 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int [] arr = {5, 3, 7, 0, 0, 4};
+        System.out.println(countEvens(arr));
+        System.out.println(difference(arr));
+        System.out.println(doubleZero(arr));
     }
     /* 1. Написать метод, возвращающий количество чётных элементов массива.
     countEvens([2, 1, 2, 3, 4]) → 3 countEvens([2, 2, 0]) → 3 countEvens([1, 3, 5]) → 0
      */
-    int countEvens(int [] arr) {
+    public static int countEvens(int [] arr) {
         int count = 0;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] %2 == 0){
@@ -18,7 +21,7 @@ public class Main {
     /* 2. Написать функцию, возвращающую разницу между самым большим и самым маленьким элементами
     переданного не пустого массива.
      */
-    int difference(int [] arr){
+    public static int difference(int [] arr){
         int min = 0;
         int max = 0;
         for (int i = 1; i < arr.length; i++) {
@@ -31,7 +34,7 @@ public class Main {
     /* 3. Написать функцию, возвращающую истину, если в переданном массиве есть два соседних
     элемента, с нулевым значением.
      */
-    boolean doubleZero(int[] arr){
+    public static boolean doubleZero(int[] arr){
         boolean flag = false;
         for (int i = 0; i < arr.length-1; i++) {
             if (arr[i] == 0 && arr[i+1] == 0) {
