e9f37673abb9619df7a76a6a4c6342b24bc2236f
vika <vmliechko@mail.ru>
2024-07-11 15:24:43 +0300

    add HW2

---
+++ HW/HW2/src/Main.java
@@ -0,0 +1,43 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+    /* 1. Написать метод, возвращающий количество чётных элементов массива.
+    countEvens([2, 1, 2, 3, 4]) → 3 countEvens([2, 2, 0]) → 3 countEvens([1, 3, 5]) → 0
+     */
+    int countEvens(int [] arr) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] %2 == 0){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    /* 2. Написать функцию, возвращающую разницу между самым большим и самым маленьким элементами
+    переданного не пустого массива.
+     */
+    int difference(int [] arr){
+        int min = 0;
+        int max = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) min = arr[i];
+            if (arr[i] > max) max = arr[i];
+        }
+        return max-min;
+    }
+
+    /* 3. Написать функцию, возвращающую истину, если в переданном массиве есть два соседних
+    элемента, с нулевым значением.
+     */
+    boolean doubleZero(int[] arr){
+        boolean flag = false;
+        for (int i = 0; i < arr.length-1; i++) {
+            if (arr[i] == 0 && arr[i+1] == 0) {
+                flag = true;
+            }
+        }
+        return flag;
+    }
+}
